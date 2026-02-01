cab22136fbadccf4eb70922061c02b3c15cea050
Enzo Gabriel Gomes <itsenzogabriel@gmail.com>
2026-02-01 01:44:40 -0300

    Engine: Adicionada função ReLU e motor de retropropagação automática

--- engine.hpp
+++ engine.hpp
@@ -3,6 +3,8 @@
 #include <memory>
 #include <functional>
 #include <cmath>
+#include <set>
+#include <algorithm>
 
 class Value : public std::enable_shared_from_this<Value> {
 public:
@@ -16,7 +18,6 @@ public:
         _backward = []() {};
     }
 
-    // Operação de Soma
     static std::shared_ptr<Value> add(std::shared_ptr<Value> a, std::shared_ptr<Value> b) {
         auto out = std::make_shared<Value>(a->data + b->data, std::vector<std::shared_ptr<Value>>{a, b});
         out->_backward = [a, b, out]() {
@@ -26,7 +27,6 @@ public:
         return out;
     }
 
-    // Operação de Multiplicação
     static std::shared_ptr<Value> mul(std::shared_ptr<Value> a, std::shared_ptr<Value> b) {
         auto out = std::make_shared<Value>(a->data * b->data, std::vector<std::shared_ptr<Value>>{a, b});
         out->_backward = [a, b, out]() {
@@ -34,5 +34,37 @@ public:
             b->grad += a->data * out->grad;
         };
         return out;
+    }
+
+    // Função de Ativação ReLU
+    static std::shared_ptr<Value> relu(std::shared_ptr<Value> a) {
+        auto out = std::make_shared<Value>(a->data < 0 ? 0 : a->data, std::vector<std::shared_ptr<Value>>{a});
+        out->_backward = [a, out]() {
+            a->grad += (out->data > 0) ? out->grad : 0;
+        };
+        return out;
+    }
+
+    // O motor de retropropagação (Autograd)
+    void backward() {
+        std::vector<std::shared_ptr<Value>> topo;
+        std::set<std::shared_ptr<Value>> visited;
+        
+        std::function<void(std::shared_ptr<Value>)> build_topo = [&](std::shared_ptr<Value> v) {
+            if (visited.find(v) == visited.end()) {
+                visited.insert(v);
+                for (auto& prev : v->_prev) {
+                    build_topo(prev);
+                }
+                topo.push_back(v);
+            }
+        };
+
+        build_topo(shared_from_this());
+        this->grad = 1.0;
+        std::reverse(topo.begin(), topo.end());
+        for (auto& v : topo) {
+            v->_backward();
+        }
     }
 };
